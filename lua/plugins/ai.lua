813515fbad59ae5df01020ec4c7d2ae6314fe552
andreybrumatti <andreyporto004@gmail.com>
2026-02-10 17:22:13 -0300

    add autocomplete

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    opts = {
+      keymaps = {
+        accept_suggestion = "<Tab>",
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-l>",
+      },
+      disable_inline_completion = false,
+    },
+  },
+}
