d5dd2cfdd778a35a3442f01e5386709dd8eae4aa
andreybrumatti <andreyporto004@gmail.com>
2026-02-10 12:39:26 -0300

    changing animation

---
+++ lua/plugins/animations.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "sphamba/smear-cursor.nvim",
+    event = "VeryLazy",
+    opts = {
+      cursor_color = "#00d4ff",
+      stiffness = 0.6,
+      trailing_stiffness = 0.3,
+      hide_target_hack = false,
+    },
+  },
+}
