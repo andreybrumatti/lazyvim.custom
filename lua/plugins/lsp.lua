85562a19cf235f5377d7cbc6df6a0bc3dbff71c1
andreybrumatti <andreyporto004@gmail.com>
2026-02-11 06:23:37 -0300

    lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,7 +3,30 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        intelephense = {},
+        intelephense = {
+          enabled = true,
+          cmd = { "intelephense", "--stdio" },
+          filetypes = { "php", "blade" },
+          root_dir = function(fname)
+            local util = require("lspconfig.util")
+            return util.root_pattern("composer.json", ".git", "artisan")(fname) or vim.fn.getcwd()
+          end,
+          settings = {
+            intelephense = {
+              diagnostics = {
+                enable = true,
+                undefinedClassDeclarations = true,
+                undefinedFunctions = true,
+                undefinedConstants = true,
+                undefinedProperties = true,
+                undefinedTypes = true,
+              },
+              completion = {
+                insertUseDeclaration = true,
+              },
+            },
+          },
+        },
         phpactor = { enabled = false },
       },
     },
