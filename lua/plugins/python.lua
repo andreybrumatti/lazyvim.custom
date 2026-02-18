d4781429a4355f61b7fa2e6e2bb274451c773a4b
andreybrumatti <andreyporto004@gmail.com>
2026-02-18 08:12:59 -0300

    feat: add configs automatic ruff

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -18,13 +18,37 @@ return {
             },
           },
         },
-        ruff = {},
+        ruff = {
+          keys = {
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.organizeImports" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Organize Imports",
+            },
+          },
+        },
       },
       setup = {
         pyright = function(_, opts)
           local venv_path = vim.fn.getcwd() .. "/.venv/bin/python"
           opts.settings.python.pythonPath = venv_path
         end,
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
       },
     },
   },
