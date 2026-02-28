78b1c9f5185f3ac0081d49dc6ca426ab246f12ec
andreybrumatti <andreyporto004@gmail.com>
2026-02-28 01:37:08 -0300

    fix: changing theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,37 +1,66 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
     priority = 1000,
     opts = {
-      flavour = "mocha",
-      color_overrides = {
-        mocha = {
-          base = "#1a1a1a",
-          mantle = "#161616",
-          crust = "#111111",
-        },
+      disable_background = false,
+      disable_float_background = false,
+      groups = {
+        background = "#121212",
+        panel = "#121212",
+        border = "#222222",
       },
-      styles = {
-        comments = {},
-        conditionals = { "bold" },
-        loops = { "bold" },
-        functions = {},
-        keywords = { "bold" },
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-      },
+      on_highlights = function(groups, color)
+        groups.Normal = { fg = "#FFFFFF", bg = "#121212" }
+
+        groups.NeoTreeNormal = { bg = "#121212" }
+        groups.NeoTreeNormalNC = { bg = "#121212" }
+      end,
     },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "poimandres",
     },
   },
 }
+
+-- return {
+--   {
+--     "catppuccin/nvim",
+--     name = "catppuccin",
+--     priority = 1000,
+--     opts = {
+--       flavour = "mocha",
+--       color_overrides = {
+--         mocha = {
+--           base = "#1a1a1a",
+--           mantle = "#161616",
+--           crust = "#111111",
+--         },
+--       },
+--       styles = {
+--         comments = {},
+--         conditionals = { "bold" },
+--         loops = { "bold" },
+--         functions = {},
+--         keywords = { "bold" },
+--         strings = {},
+--         variables = {},
+--         numbers = {},
+--         booleans = {},
+--         properties = {},
+--         types = {},
+--         operators = {},
+--       },
+--     },
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin",
+--     },
+--   },
+-- }
